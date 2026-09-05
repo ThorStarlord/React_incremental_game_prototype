0534bd619ec27ec22802ce38a7ac35ce0efcc9cb
ThorStarlord <140429883+ThorStarlord@users.noreply.github.com>
2026-09-05 09:34:57 -0300

    test: align jest-dom type augmentation with installed package

--- src/jest-dom.d.ts
+++ src/jest-dom.d.ts
@@ -1,12 +1,1 @@
-import '@testing-library/jest-dom/extend-expect';
-
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeEnabled(): R;
-      toBeInTheDocument(): R;
-    }
-  }
-}
-
-export {};
+import '@testing-library/jest-dom';
