bb10e970a5db8b6bc0ec32da9e021d92b7134a25
ThorStarlord <140429883+ThorStarlord@users.noreply.github.com>
2026-09-05 09:32:22 -0300

    test: declare legacy jest-dom matchers for tsc

--- src/jest-dom.d.ts
+++ src/jest-dom.d.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom/extend-expect';
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeEnabled(): R;
+      toBeInTheDocument(): R;
+    }
+  }
+}
+
+export {};
