aa993d10f848d4dfbb76e0f4ac92fd22330695ad
ThorStarlord <140429883+ThorStarlord@users.noreply.github.com>
2026-09-08 05:31:14 -0300

    feat(m23): export faction authority

---
+++ src/features/Factions/index.ts
@@ -0,0 +1,4 @@
+export { default as factionReducer } from './state/FactionSlice';
+export { adjustFactionReputation, resetFactionReputation } from './state/FactionSlice';
+export { selectFactionReputation } from './state/FactionSelectors';
+export type { FactionState, FactionReputationChange } from './state/FactionTypes';
