adaa55ead6cc2aeaee3f6fd1164cdbc213aba532
ThorStarlord <140429883+ThorStarlord@users.noreply.github.com>
2026-09-23 16:14:28 -0300

    Keep GameLoop qualification aligned with standing orders

--- src/features/GameLoop/GameLoopBackpressureCadenceProgressionStressQualification.test.tsx
+++ src/features/GameLoop/GameLoopBackpressureCadenceProgressionStressQualification.test.tsx
@@ -10,6 +10,7 @@ import {
   processCopyLoyaltyDecayThunk,
   processCopyTasksThunk,
 } from '../Copy/state/CopyThunks';
+import { processCopyStandingOrdersThunk } from '../Copy/state/CopyStrategyThunks';
 import { updateCopy } from '../Copy/state/CopySlice';
 import {
   processStatusEffectsThunk,
@@ -138,6 +139,7 @@ const runOnlineProgressionTick = async (store: TestStore, tickData: TickData) =>
   await store.dispatch(processCopyGrowthThunk(tickData.deltaTime));
   await store.dispatch(processCopyLoyaltyDecayThunk(tickData.deltaTime));
   await store.dispatch(processCopyTasksThunk(tickData.deltaTime));
+  await store.dispatch(processCopyStandingOrdersThunk());
   store.dispatch(processResonanceLevelThunk());
   await store.dispatch(processStatusEffectsThunk());
   await store.dispatch(regenerateVitalsThunk(tickData.deltaTime));
